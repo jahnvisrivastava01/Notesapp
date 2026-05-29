df345b2e40d9e87fc5976fc4bc4ea47bab4b208a
Jahnvi Srivastava <jahnvisrivastava0104@gmail.com>
2026-05-29 20:26:14 +0530

    Initial commit - Personal Notes App

---
+++ script.js
@@ -0,0 +1,84 @@
+const noteForm = document.getElementById("noteForm");
+const noteInput = document.getElementById("noteInput");
+const notesContainer = document.getElementById("notesContainer");
+
+let notes = JSON.parse(localStorage.getItem("notes")) || [];
+
+displayNotes();
+
+noteForm.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const noteText = noteInput.value.trim();
+
+    if(noteText === ""){
+        return;
+    }
+
+    const note = {
+        text: noteText,
+        date: new Date().toLocaleString()
+    };
+
+    notes.unshift(note);
+
+    localStorage.setItem(
+        "notes",
+        JSON.stringify(notes)
+    );
+
+    noteInput.value = "";
+
+    displayNotes();
+});
+
+function displayNotes(){
+
+    notesContainer.innerHTML = "";
+
+    if(notes.length === 0){
+
+        notesContainer.innerHTML =
+        `<p>No notes available.</p>`;
+
+        return;
+    }
+
+    notes.forEach((note,index)=>{
+
+        const noteCard = document.createElement("div");
+
+        noteCard.classList.add("note-card");
+
+        noteCard.innerHTML = `
+            <p>${note.text}</p>
+
+            <div class="note-date">
+                ${note.date}
+            </div>
+
+            <button
+                class="delete-btn"
+                onclick="deleteNote(${index})"
+            >
+                Delete
+            </button>
+        `;
+
+        notesContainer.appendChild(noteCard);
+
+    });
+}
+
+function deleteNote(index){
+
+    notes.splice(index,1);
+
+    localStorage.setItem(
+        "notes",
+        JSON.stringify(notes)
+    );
+
+    displayNotes();
+}
